e2d03585851171ccfc8949c9dc55a11f7adad69a
Diego Cardoso <dcardoso86@gmail.com>
2018-08-12 22:55:39 -0300

    Initial commit

---
+++ app/src/main/java/br/com/opet/tds/pokeapiapp/Model/Pokemon.java
@@ -0,0 +1,93 @@
+package br.com.opet.tds.pokeapiapp.Model;
+
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by Diego on 12/08/2018.
+ */
+
+public class Pokemon {
+
+    @SerializedName("id")
+    private long ID;
+    private String name;
+    private int height;
+    private int weight;
+    private List<Types> types;
+
+    public Pokemon() {
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public List<Types> getTypes() {
+        return types;
+    }
+
+    public void setTypes(List<Types> types) {
+        this.types = types;
+    }
+
+    public class Types{
+        private Type type;
+
+        public Types() {
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        public void setType(Type type) {
+            this.type = type;
+        }
+
+        public class Type{
+            private String name;
+
+            public Type() {
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+    }
+}
